6f31d35d327ba8b8beb28ffc240ce5938f84f74f
Savan  Awanti <savan.awanti@happiestminds.com>
2023-02-14 16:43:38 +0530

    solved

--- test3.rb
+++ test3.rb
@@ -1,7 +1,22 @@
 class Test 
 
     def fun(input)
-        
+
+        answer={}
+        k=0
+        count=1
+        hcount=1
+        while k<=input.length
+            if input[k]==input[k+1]
+                count=count+1
+            else
+                answer["#{input[k]} #{hcount}"]=count
+                hcount=hcount+1
+                count=1
+            end
+            k=k+1
+        end
+        puts answer
 
     end
 
