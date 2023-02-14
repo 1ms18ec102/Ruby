ff63b06f179600b787e2acfbe372835221ce3cfe
Savan  Awanti <savan.awanti@happiestminds.com>
2023-02-14 16:33:32 +0530

    solved

--- test1.rb
+++ test1.rb
@@ -1,1 +1,23 @@
-puts "hello_world"
+class Test 
+
+    def fun(input)
+
+        if check(input)==1.0
+            puts "odd"
+        else
+            puts "even"
+        end
+    end
+    
+    def check(number)
+        a=number/2
+        b=a-a.to_i
+        answer=b*2
+        return answer
+    end
+end
+
+
+test=Test.new
+input = gets.chomp.to_f
+test.fun(input)
